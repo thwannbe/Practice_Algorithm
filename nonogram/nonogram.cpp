d28b2c1419c3ea6056ab2fbfd45b9ed7574a53e5
Soowon Oh <osw5144@nate.com>
2021-03-13 14:18:59 +0900

    nonogram ver2

--- nonogram/nonogram.cpp
+++ nonogram/nonogram.cpp
@@ -9,9 +9,11 @@ int line_size;
 
 struct line_info {
 	vector<int> info;
-	vector<int[20]> candidate;
+	vector<int[MAX_LINE_SIZE]> candidate;
 	bool done;
 } rows[MAX_LINE_SIZE], cols[MAX_LINE_SIZE];
+
+int answer[MAX_LINE_SIZE][MAX_LINE_SIZE];
 
 void get_input(void) {
 	cout << "size: ";
@@ -40,11 +42,79 @@ void get_input(void) {
 			rows[i].info.push_back(tmp);
 		}
 	}
+
+	// init answer
+	for (int i = 0; i < line_size; i++)
+		for (int j = 0; j < line_size; j++)
+			answer[i][j] = -1; // init val
 }
 
-void init_search(void) {
-	// find possible line to be done with initial information
-	// doing until no more update
+void copy_line(const int src[MAX_LINE_SIZE], int dst[MAX_LINE_SIZE]) {
+	for (int i = 0; i < line_size; i++)
+		dst[i] = src[i];
+}
+
+void fill_line(int line[MAX_LINE_SIZE], int start_idx, int size) {
+	for (int i = 0; i < size; i++)
+		line[start_idx + i] = 1;	
+}
+
+void clear_line(int line[MAX_LINE_SIZE], int start_idx, int size) {
+	for (int i = 0; i < size; i++)
+		line[start_idx + i] = 0;
+}
+
+void recur_gen(vector<int> info, vector<int[MAX_LINE_SIZE]> candidate, 
+		int info_idx, int next_board_ptr, int[MAX_LINE_SIZE] cur_board) {
+	if (info_idx == info.size()) {
+		// all consumed
+		int temp[MAX_LINE_SIZE];
+		copy_line(cur_board, temp);
+		candidate.push_back(temp);
+	} else {
+		for (int i = next_board_ptr; i < line_size; i++) {
+			if (i + info[info_idx] <= line_size) {
+				fill_line(cur_board, i, info[info_idx]);
+				recur_gen(info, candidate, info_idx + 1,
+						i + info[info_idx] + 1, cur_board);
+				clear_line(cur_board, i, info[info_idx]);
+			} else
+				break;
+		}
+	}
+}
+
+void gen_candidate(int index, bool isRow) {
+	int temp[MAX_LINE_SIZE] = {0,};
+	recur_gen(isRow ? rows[index].info : cols[index].info, \
+		  isRow ? rows[index].candidate : cols[index].candidate, \
+		  0, 0, temp);
+}
+
+void copy_cand_to_answer(int index, const int cand[20], bool isRow) {
+	if (isRow) {
+		for (int i = 0; i < line_size; i++) {
+			answer[index][i] = cand[i];
+		}
+	} else {
+		for (int i = 0; i < line_size; i++) {
+			answer[i][index] = cand[i];
+	}
+}
+
+void fill_candidate(void) {
+	// add all candidate for each rows and columns
+	// if anything has only one candidate, it must be answer.
+	for (int i = 0; i < line_size; i++) {
+		// for row
+		gen_candidate(i, true);
+		if (rows[i].candidate.size() == 1)
+			copy_cand_to_answer(i, (const int[20])rows[i].candidate[0], true);
+		// for cols
+		gen_candidate(i, false);
+		if (cols[i].candidate.size() == 1)
+			copy_cand_to_answer(i, (const int[20])cols[i].candidate[0], true);
+	}
 }
 
 void regression(void) {
@@ -58,7 +128,7 @@ void print_answer() {
 int main()
 {
 	get_input();
-	init_search();
+	fill_candidate();
 	regression();
 	print_answer();
 }
